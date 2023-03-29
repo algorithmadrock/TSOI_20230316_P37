2fe6d8de527dec9733b3d70c38a703231d68d21b
luiza <luizamfelix@gmail.com>
2023-03-29 18:48:38 -0300

    Atualização 29/03/2023

---
+++ src/controller/TC.java
@@ -0,0 +1,44 @@
+/*
+RESUMO      : Exercicio da pg.37, Simulação de um cruzamento de carros. Classe dos carros
+PROGRAMADORA: Luiza Felix
+DATA        : 29/03/2023
+ */
+
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+//TC = Thread Carro
+public class TC extends Thread {
+	
+	private String sentido;
+	private Semaphore semaforo = new Semaphore(1);
+	
+	public TC(String sentido) {
+		this.sentido = sentido;
+		
+	}
+
+	@Override
+	public void run() {
+		try {
+			semaforo.acquire(); // é mutex por padrão
+			cruzamento();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+	}
+
+	private void cruzamento() {
+		try {
+//			simulando a passagem do carro, bem rapidinho
+			sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("O carro que estava indo sentido " + sentido + " cruzou.");
+		
+	}
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,23 @@
+/*
+RESUMO      : Exercicio da pg.37, Simulação de um cruzamento de carros. Classe Principal
+PROGRAMADORA: Luiza Felix
+DATA        : 29/03/2023
+ */
+
+package view;
+
+import controller.TC;
+
+public class Principal {
+	public static void main(String[] args) {	
+		
+		String[] sentidos = {"norte", "sul", "leste", "oeste"};
+		
+		for (int i = 0; i<4; i++) {		
+			Thread thread = new TC(sentidos[i]);
+			
+			thread.start();
+		}
+	}
+
+}
